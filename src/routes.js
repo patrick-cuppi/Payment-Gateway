634cb298dbd32c5e378dc747aa78213f2144f550
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-05-26 15:14:25 -0300

    feat: config server

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import CartsController from "./controllers/CartsController";
+
+const routes = new Router()
+
+routes.get('/carts', CartsController.index)
+
+export default routes
---
+++ src/server.js
@@ -0,0 +1,4 @@
+import "dotenv/config";
+import App from "./App";
+
+App.listen(process.env.PORT)
