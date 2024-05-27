429fa9179bef52c44251298ccb66885469ec0754
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-05-27 19:45:23 -0300

    feat: transaction controller created

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import CartsController from "./controllers/CartsController";
+import TransactionsController from "./controllers/TransactionsController";
 
 const routes = new Router();
 
@@ -7,5 +8,7 @@ routes.get('/carts', CartsController.index);
 routes.post('/carts', CartsController.create);
 routes.put('/carts/:id', CartsController.update);
 routes.delete('/carts/:id', CartsController.destroy);
+
+routes.post('/transactions', TransactionsController.create);
 
 export default routes
